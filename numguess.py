d7ba1a718df72d2aaa0d11375965bb55e63f605c
dhtkddbs01 <dhtkddbs2@gmail.com>
2023-02-03 13:45:12 +0900

    feat: get your user name, number

--- numguess.py
+++ numguess.py
@@ -3,3 +3,11 @@ from random import randint
 # create answer range
 answer = randint(1,100)
 print(answer)
+
+# get user's name, guess
+
+user_name = input('hello, there! what is your name?'
+guess = input(f'hi. {user_name}. guess the number(1-100): ')
+
+# print to check
+print(user_name, guess)
