20d35fecf3065c075c2afd4fe16bfcb1d57e55a0
dhtkddbs01 <dhtkddbs2@gmail.com>
2023-02-03 13:50:30 +0900

    feat: compare and print answer

--- numguess.py
+++ numguess.py
@@ -11,3 +11,10 @@ guess = input(f'hi. {user_name}. guess the number(1-100): ')
 
 # print to check
 print(user_name, guess)
+
+# check and print correct or not
+if guess==answer:
+    print('congrats!')
+else:
+    print(f"you are wrong! the answer was {answer}.")
+
